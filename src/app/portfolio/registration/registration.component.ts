6fca4cb907d4c01248d1af76d7982dee4d3f47c4
Abhishek Upadhyaya <abhishekupadhyaya477@gmail.com>
2024-02-25 01:58:12 +0530

    added registration page changes

--- src/app/portfolio/registration/registration.component.ts
+++ src/app/portfolio/registration/registration.component.ts
@@ -1,15 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon'
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [MatDialogModule,CommonModule,MatIconModule],
+  imports: [MatDialogModule,CommonModule,MatIconModule,ReactiveFormsModule,MatFormFieldModule,MatInputModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent implements OnInit{
+  hide=true;
+  registrationForm: FormGroup=new FormGroup(
+    {
+      name:new FormControl('',Validators.required),
+      email:new FormControl('',Validators.required),
+      password:new FormControl('',Validators.required)
+    }
+  );
 ngOnInit(): void {
   
 }
@@ -22,6 +34,9 @@ toggleSignUpClass(signUp: boolean): void {
   else{
     document.getElementById('container')?.classList.remove("right-panel-active")
   }
+}
+register(){
+  console.log(this.registrationForm.value);
 }
 }
 
