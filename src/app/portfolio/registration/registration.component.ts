cb552b4dbddf2e56e9cf6a3aa402961032c631f1
Abhishek Upadhyaya <abhishekupadhyaya477@gmail.com>
2024-02-25 13:57:59 +0530

    fireAuth added

--- src/app/portfolio/registration/registration.component.ts
+++ src/app/portfolio/registration/registration.component.ts
@@ -5,11 +5,14 @@ import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon'
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { AuthService } from '../../services/auth.service';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [MatDialogModule,CommonModule,MatIconModule,ReactiveFormsModule,MatFormFieldModule,MatInputModule],
+  imports: [MatDialogModule,CommonModule,MatIconModule,ReactiveFormsModule,MatFormFieldModule,MatInputModule,AngularFireAuthModule],
+  providers:[AuthService],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
@@ -22,6 +25,7 @@ export class RegistrationComponent implements OnInit{
       password:new FormControl('',Validators.required)
     }
   );
+  constructor(private authService:AuthService) { }
 ngOnInit(): void {
   
 }
@@ -36,7 +40,8 @@ toggleSignUpClass(signUp: boolean): void {
   }
 }
 register(){
-  console.log(this.registrationForm.value);
+  this.authService.signIn(this.registrationForm.value)
+  // console.log(this.registrationForm.value);
 }
 }
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-
-@Injectable({
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+ @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor( private auth: AngularFireAuth) { }
+  signIn(user:any){
+    return this.auth.signInWithEmailAndPassword(user.email,user.password);
+  }
 }
